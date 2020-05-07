4282921bd3780b26d05ee6581f7f4ee371f13edb
daniyal nagori <daniyalnagori@yahoo.com>
2020-05-07 16:08:54 +0500

    adde1dasd

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "account_name" {
+  default = "daniyalnagori"
+}
+
+variable "github_org" {
+  default = "daniyalnagori"
+}
+
+variable "github_token" {
+  default = "1250e891331d27ac1860e440732596ac34b8d00b"
+}
+
+variable "project" {
+	default = "terraform-codepipeline-example"
+}
+
+variable "app" {
+	default = "terraform-codepipeline-example"
+}
+
+variable "account_id" {
+}
+
+variable "docker_build_image" {
+  default = "ubuntu"
+}
